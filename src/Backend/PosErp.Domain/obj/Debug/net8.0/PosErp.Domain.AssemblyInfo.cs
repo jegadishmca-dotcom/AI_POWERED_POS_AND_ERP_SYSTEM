6e9197efa85a700e23194d52df0f140b41d3c396
jegadishmca-dotcom <jegadishmca@gmail.com>
2026-06-05 01:13:47 +0530

    Fix: Create invoices database partitions and default fallback partitions

--- src/Backend/PosErp.Domain/obj/Debug/net8.0/PosErp.Domain.AssemblyInfo.cs
+++ src/Backend/PosErp.Domain/obj/Debug/net8.0/PosErp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosErp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f711d98ad949facbeb7e96acf4686e974ae6db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80aafa4e690f33108f482d42111359f8edfd2ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosErp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PosErp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Backend/PosErp.Infrastructure/obj/Debug/net8.0/PosErp.Infrastructure.AssemblyInfo.cs
+++ src/Backend/PosErp.Infrastructure/obj/Debug/net8.0/PosErp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosErp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f711d98ad949facbeb7e96acf4686e974ae6db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80aafa4e690f33108f482d42111359f8edfd2ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosErp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PosErp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

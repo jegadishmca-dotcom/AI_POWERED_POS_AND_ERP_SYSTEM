bbba204a6376b30fca3cc4a53ab34014868ea76b
jegadishmca-dotcom <jegadishmca@gmail.com>
2026-07-03 12:21:17 +0530

    Revert "Expose ProcessInterStoreTransferCommand in FinanceController for staging smoke test verification"
    This reverts commit e43d79bec4a6818b9dd952e95b7264f8da2a64ac.

--- src/Backend/PosErp.Api/Controllers/FinanceController.cs
+++ src/Backend/PosErp.Api/Controllers/FinanceController.cs
@@ -2,7 +2,6 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PosErp.Application.Features.Finance.Queries;
-using PosErp.Application.Features.Finance.Commands;
 using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -27,24 +26,5 @@ public class FinanceController : ControllerBase
         var activeStoreId = storeId ?? Guid.Parse("00000000-0000-0000-0000-000000000000");
         var result = await _mediator.Send(new GetFinanceDashboardQuery(activeStoreId));
         return Ok(result);
-    }
-
-    [HttpPost("transfers")]
-    public async Task<IActionResult> ProcessTransfer([FromBody] ProcessInterStoreTransferCommand command)
-    {
-        var callerIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (!Guid.TryParse(callerIdStr, out Guid userId))
-        {
-            return Unauthorized("User ID claim not found.");
-        }
-        var updatedCommand = new ProcessInterStoreTransferCommand(
-            command.FromStoreId,
-            command.ToStoreId,
-            command.TransferDate,
-            command.Items,
-            userId
-        );
-        var id = await _mediator.Send(updatedCommand);
-        return Ok(new { id });
     }
 }
